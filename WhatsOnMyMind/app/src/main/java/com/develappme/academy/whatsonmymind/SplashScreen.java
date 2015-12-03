61e3e23e029b6c8f63009ae94c2910b4cb1d77a8
Raquel <rlawrenc@ggc.edu>
2015-12-03 14:59:47 -0500

    init

---
+++ WhatsOnMyMind/app/src/main/java/com/develappme/academy/whatsonmymind/SplashScreen.java
@@ -0,0 +1,36 @@
+package com.develappme.academy.whatsonmymind;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static final int SPLASH_TIMEOUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        }, SPLASH_TIMEOUT);
+
+    }
+
+}
