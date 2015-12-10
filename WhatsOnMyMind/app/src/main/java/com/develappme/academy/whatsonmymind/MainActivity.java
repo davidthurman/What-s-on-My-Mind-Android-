ea6fc44256711b8dc18bf9c9c50b032c0d056a44
David Thurman <davidthurmanwork@gmail.com>
2015-12-10 11:28:03 -0500

    Updated Choosing From Gallery

--- WhatsOnMyMind/app/src/main/java/com/develappme/academy/whatsonmymind/MainActivity.java
+++ WhatsOnMyMind/app/src/main/java/com/develappme/academy/whatsonmymind/MainActivity.java
@@ -1,22 +1,33 @@
 package com.develappme.academy.whatsonmymind;
 
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraCharacteristics;
-import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CaptureRequest;
 import android.hardware.camera2.params.StreamConfigurationMap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Size;
 import android.view.Surface;
 import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -37,6 +48,8 @@ public class MainActivity extends AppCompatActivity {
     private CaptureRequest.Builder mPreviewCaptureRequestBuilder;
     private CameraCaptureSession mCameraCaptureSession;
     private CameraCaptureSession.CaptureCallback mSessionCaptureCallback;
+    private Button fromCamera;
+    private Button fromPictures;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +57,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        fromPictures = (Button) findViewById(R.id.choosePhotoBtn);
+        fromPictures.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Testing");
+                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, 1);
+            }
+        });
 
         mTextureView = (TextureView) findViewById(R.id.textureView);
 
@@ -102,17 +125,30 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void setupCamera(int width, int height)
-    {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null){
+            Uri selectedImage = data.getData();
+            try {
+                Bitmap bitMapImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                //ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                //imageView.setImageBitmap(bitMapImage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public void setupCamera(int width, int height) {
         cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-        try
-        {
-            for(String cameraId : cameraManager.getCameraIdList())
-            {
+        try {
+            for (String cameraId : cameraManager.getCameraIdList()) {
                 CameraCharacteristics cameraCharacteristics = cameraManager.getCameraCharacteristics(cameraId);
-                if(cameraCharacteristics.get(cameraCharacteristics.LENS_FACING) ==
-                        CameraCharacteristics.LENS_FACING_FRONT)
-                {
+                if (cameraCharacteristics.get(cameraCharacteristics.LENS_FACING) ==
+                        CameraCharacteristics.LENS_FACING_FRONT) {
                     continue;
                 }
 
@@ -122,32 +158,26 @@ public class MainActivity extends AppCompatActivity {
                 return;
             }
 
-        } catch(CameraAccessException e){
+        } catch (CameraAccessException e) {
             e.printStackTrace();
         }
     }
 
-    private Size getPreferredPreviewSize(Size[] mapSizes, int width, int height)
-    {
+    private Size getPreferredPreviewSize(Size[] mapSizes, int width, int height) {
         List<Size> collectorSizes = new ArrayList<>();
-        for(Size option : mapSizes)
-        {
-            if(width > height)
-            {
-                if(option.getWidth() > width && option.getHeight() > height)
-                {
+        for (Size option : mapSizes) {
+            if (width > height) {
+                if (option.getWidth() > width && option.getHeight() > height) {
                     collectorSizes.add(option);
                 }
-            } else{
-                if(option.getWidth() > height && option.getHeight() > width)
-                {
+            } else {
+                if (option.getWidth() > height && option.getHeight() > width) {
                     collectorSizes.add(option);
                 }
             }
         }
 
-        if(!collectorSizes.isEmpty())
-        {
+        if (!collectorSizes.isEmpty()) {
             Collections.min(collectorSizes, new Comparator<Size>() {
                 @Override
                 public int compare(Size lhs, Size rhs) {
@@ -159,11 +189,22 @@ public class MainActivity extends AppCompatActivity {
         return mapSizes[0];
     }
 
-    private void openCamera()
-    {
+    private void openCamera() {
         CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
 
-        try{
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    public void requestPermissions(@NonNull String[] permissions, int requestCode)
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for Activity#requestPermissions for more details.
+                    return;
+                }
+            }
             cameraManager.openCamera(mCameraId, mCameraDeviceStateCallback, null);
 
         } catch(CameraAccessException e){
@@ -225,6 +266,7 @@ public class MainActivity extends AppCompatActivity {
             mTextureView.setSurfaceTextureListener(mSurfaceTextureListener);
         }
     }
+
 }
 
 
