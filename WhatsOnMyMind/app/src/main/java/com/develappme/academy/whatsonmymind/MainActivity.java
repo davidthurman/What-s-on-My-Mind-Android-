2af1e62cabc83f916da58b08960da529fc535830
Raquel <rlawrenc@ggc.edu>
2015-12-10 17:04:16 -0500

    take photo button working

--- WhatsOnMyMind/app/src/main/java/com/develappme/academy/whatsonmymind/MainActivity.java
+++ WhatsOnMyMind/app/src/main/java/com/develappme/academy/whatsonmymind/MainActivity.java
@@ -1,22 +1,19 @@
 package com.develappme.academy.whatsonmymind;
 
+
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.ImageFormat;
 import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraCharacteristics;
-import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CaptureFailure;
 import android.hardware.camera2.CaptureRequest;
 import android.hardware.camera2.CaptureResult;
 import android.hardware.camera2.TotalCaptureResult;
 import android.hardware.camera2.params.StreamConfigurationMap;
-import android.media.Image;
-import android.media.ImageReader;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
@@ -24,16 +21,11 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Size;
-import android.util.SparseIntArray;
 import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -43,13 +35,6 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
-    static{
-        ORIENTATIONS.append(Surface.ROTATION_0, 90);
-        ORIENTATIONS.append(Surface.ROTATION_90, 0);
-        ORIENTATIONS.append(Surface.ROTATION_180, 270);
-        ORIENTATIONS.append(Surface.ROTATION_270, 180);
-    }
     private CameraManager cameraManager;
     private TextureView mTextureView;
     private TextureView.SurfaceTextureListener mSurfaceTextureListener;
@@ -66,15 +51,8 @@ public class MainActivity extends AppCompatActivity {
     private static final int STATE_PREVIEW = 0;
     private static final int STATE_WAIT_LOCK = 1;
     private int mState;
-    private static File mImageFile;
-    private ImageReader mImageReader;
-    private final ImageReader.OnImageAvailableListener mOnImageAvailableListener =
-            new ImageReader.OnImageAvailableListener() {
-                @Override
-                public void onImageAvailable(ImageReader reader) {
-                    mBackgroundHandler.post(new ImageSaver(reader.acquireNextImage()));
-                }
-            };
+    private static final int ACTIVITY_START_CAMERA_APP = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -93,7 +71,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // Choose Photo Button!
         Button fromPictures = (Button) findViewById(R.id.choosePhotoBtn);
         fromPictures.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -103,7 +80,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
         mTextureView = (TextureView) findViewById(R.id.textureView);
+
         mSurfaceTextureListener = new TextureView.SurfaceTextureListener() {
             @Override
             public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
@@ -160,7 +139,8 @@ public class MainActivity extends AppCompatActivity {
                     case STATE_WAIT_LOCK:
                         Integer afState = result.get(CaptureResult.CONTROL_AF_STATE);
                         if(afState == CaptureResult.CONTROL_AF_STATE_FOCUSED_LOCKED) {
-                            captureStillImage();
+                            unlockFocus();
+                            Toast.makeText(getApplicationContext(), "Focus Locked Successful", Toast.LENGTH_SHORT).show();
                         }
                         break;
                 }
@@ -185,9 +165,23 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-
     }
 
+    public void takePhoto(){
+        Intent callCameraApplicationIntent = new Intent();
+        callCameraApplicationIntent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(callCameraApplicationIntent, ACTIVITY_START_CAMERA_APP);
+        lockFocus();
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == ACTIVITY_START_CAMERA_APP) {
+                //Uri selectedImageUri = data.getData();
+                startActivity(new Intent(MainActivity.this, Polaroid.class));
+            }
+        }
+    }
 
     public void setupCamera(int width, int height)
     {
@@ -204,21 +198,6 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 StreamConfigurationMap map = cameraCharacteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
-
-                // Image capture size
-                Size largestImageSize = Collections.max(Arrays.asList(map.getOutputSizes(ImageFormat.JPEG)),
-                        new Comparator<Size>() {
-                            @Override
-                            public int compare(Size lhs, Size rhs) {
-                                return Long.signum(lhs.getWidth() + lhs.getHeight() -
-                                        rhs.getWidth() * rhs.getHeight());
-                            }
-                        }
-                );
-                mImageReader = ImageReader.newInstance(largestImageSize.getWidth(), largestImageSize.getHeight(),
-                        ImageFormat.JPEG, 1);
-                mImageReader.setOnImageAvailableListener(mOnImageAvailableListener, mBackgroundHandler);
-
                 mPreviewSize = getPreferredPreviewSize(map.getOutputSizes(SurfaceTexture.class), width, height);
                 mCameraId = cameraId;
                 return;
@@ -286,13 +265,6 @@ public class MainActivity extends AppCompatActivity {
             mCameraDevice.close();
             mCameraDevice = null;
         }
-
-        if(mImageReader != null)
-        {
-            mImageReader.close();
-            mImageReader = null;
-
-        }
     }
 
     private void createCameraPreviewSession()
@@ -304,7 +276,7 @@ public class MainActivity extends AppCompatActivity {
             Surface previewSurface = new Surface(surfaceTexture);
             mPreviewCaptureRequestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
             mPreviewCaptureRequestBuilder.addTarget(previewSurface);
-            mCameraDevice.createCaptureSession(Arrays.asList(previewSurface, mImageReader.getSurface()),
+            mCameraDevice.createCaptureSession(Arrays.asList(previewSurface),
                     new CameraCaptureSession.StateCallback() {
                         @Override
                         public void onConfigured(CameraCaptureSession session) {
@@ -335,10 +307,6 @@ public class MainActivity extends AppCompatActivity {
         } catch(CameraAccessException e){
             e.printStackTrace();
         }
-    }
-
-    public void takePhoto(){
-        lockFocus();
     }
 
     private void openBackgroundThread(){
@@ -384,32 +352,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void captureStillImage()
-    {
-        try{
-            CaptureRequest.Builder captureStillBuilder = mCameraDevice.createCaptureRequest
-                    (CameraDevice.TEMPLATE_STILL_CAPTURE);
-            captureStillBuilder.addTarget(mImageReader.getSurface());
-            int rotation = getWindowManager().getDefaultDisplay().getRotation();
-            captureStillBuilder.set(CaptureRequest.JPEG_ORIENTATION,
-                    ORIENTATIONS.get(rotation));
-            CameraCaptureSession.CaptureCallback captureCallback =
-                    new CameraCaptureSession.CaptureCallback() {
-                        @Override
-                        public void onCaptureCompleted(CameraCaptureSession session, CaptureRequest request, TotalCaptureResult result) {
-                            super.onCaptureCompleted(session, request, result);
-                            Toast.makeText(getApplicationContext(), "Image Captured", Toast.LENGTH_SHORT).show();
-                            unlockFocus();
-                        }
-                    };
-            mCameraCaptureSession.capture(captureStillBuilder.build(), captureCallback, null);
-
-        } catch(CameraAccessException e){
-            e.printStackTrace();
-
-        }
-    }
-
     @Override
     protected void onResume()
     {
@@ -434,44 +376,4 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private static class ImageSaver implements Runnable{
-
-        private final Image mImage;
-
-        private ImageSaver(Image image)
-        {
-            mImage = image;
-        }
-
-        @Override
-        public void run() {
-
-            ByteBuffer byteBuffer = mImage.getPlanes()[0].getBuffer();
-            byte[] bytes = new byte[byteBuffer.remaining()];
-            byteBuffer.get(bytes);
-
-            FileOutputStream fileOutputStream = null;
-
-            try {
-                fileOutputStream = new FileOutputStream(mImageFile);
-                fileOutputStream.write(bytes);
-            } catch (IOException e){
-                e.printStackTrace();
-            } finally {
-                mImage.close();
-                if(fileOutputStream != null)
-                {
-                    try {
-                        fileOutputStream.close();
-                    } catch(IOException e){
-                        e.printStackTrace();
-                    }
-                }
-            }
-
-        }
-    }
 }
-
-
-
